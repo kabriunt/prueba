e134114de8dcc4d3e3d8108c04e2578be679cdd2
dcabrera <dcabrera@AT-GF1V5M2.atsistemas.lan>
2018-02-06 11:15:12 +0100

    Clase Producto

--- src/main/java/atsistemas/prueba/model/Product.java
+++ src/main/java/atsistemas/prueba/model/Product.java
@@ -1,5 +1,31 @@
 package atsistemas.prueba.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
 public class Product {
 
+	@Id
+	@GeneratedValue
+	private String id;
+	
+	private String name;
+	
+	@ManyToMany(fetch=FetchType.LAZY, mappedBy="products")
+	private List<Restaurant> restaurants = new ArrayList<>();
+	
+	@ManyToMany(fetch=FetchType.LAZY, mappedBy="products")
+	private List<Sale> sales = new ArrayList<>();
 }
