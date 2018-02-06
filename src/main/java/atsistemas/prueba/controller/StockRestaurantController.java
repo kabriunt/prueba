6c959eeb994650c1890a34e0f1bd13cfb17dae57
dcabrera <dcabrera@AT-GF1V5M2.atsistemas.lan>
2018-02-06 15:10:14 +0100

    Capa Controlador

---
+++ src/main/java/atsistemas/prueba/controller/StockRestaurantController.java
@@ -0,0 +1,13 @@
+package atsistemas.prueba.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping(value="/api/stockRestaurant")
+public class StockRestaurantController {
+
+}
