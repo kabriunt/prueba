7e41fa752780e2da692e8af16d5b2e3efed97882
ftorrejon <ftorrejon@AT-8J1V5M2.atsistemas.lan>
2018-02-06 11:19:53 +0100

    Modelo de Employee con atributos y relaciones

--- src/main/java/atsistemas/prueba/model/Employee.java
+++ src/main/java/atsistemas/prueba/model/Employee.java
@@ -1,5 +1,39 @@
 package atsistemas.prueba.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
 public class Employee {
+	
+	@Id
+	@GeneratedValue
+	private String id;
+	
+	private String name;
+	
+	private String email;
+	
+	private String phone;
+	
+	// Relación muchos empleados pertenecen a un único restaurante
+	@ManyToOne (fetch = FetchType.LAZY)
+	private Restaurant restaurant;
+	
+	// Relación 1 a n de empleado con ventas
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "employee")
+	private List<Sale> sales = new ArrayList<>();
 
 }
