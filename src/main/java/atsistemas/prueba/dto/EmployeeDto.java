bd9abdaaf14a1c6032b79fd9a849f98921d30235
ftorrejon <ftorrejon@AT-8J1V5M2.atsistemas.lan>
2018-02-06 17:15:58 +0100

    Crud Employee

--- src/main/java/atsistemas/prueba/dto/EmployeeDto.java
+++ src/main/java/atsistemas/prueba/dto/EmployeeDto.java
@@ -2,8 +2,32 @@ package atsistemas.prueba.dto;
 
 import java.io.Serializable;
 
-public class EmployeeDto  implements Serializable{
+import atsistemas.prueba.model.Restaurant;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmployeeDto implements Serializable {
 
 	private static final long serialVersionUID = 668545598518153456L;
+
+	private Integer id, restaurant;
+
+	private String name;
+	
+	private String email;
+	
+	private String phone;
+	
+	public EmployeeDto (Integer id, Restaurant restaurant, String name, String email, String phone) {
+		this.id=id;
+		this.restaurant = restaurant.getId();
+		this.name = name;
+		this.email = email;
+		this.phone = phone;
+	}
+
+	public EmployeeDto() {}
 
 }
--- src/main/java/atsistemas/prueba/dto/SaleDto.java
+++ src/main/java/atsistemas/prueba/dto/SaleDto.java
@@ -1,9 +1,28 @@
 package atsistemas.prueba.dto;
 
 import java.io.Serializable;
+import java.util.Date;
 
+
+import atsistemas.prueba.model.Employee;
+import atsistemas.prueba.model.Restaurant;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SaleDto  implements Serializable{
 
 	private static final long serialVersionUID = -3580071187275557767L;
 
+	private Integer id, restaurant, employee;
+	
+	private Date saledate;
+	
+	public SaleDto (Integer id, Restaurant restaurant, Employee employee, Date saledate) {
+		this.id=id;
+		this.restaurant = restaurant.getId();
+		this.employee = employee.getId();
+		this.saledate = saledate;
+	}
 }
