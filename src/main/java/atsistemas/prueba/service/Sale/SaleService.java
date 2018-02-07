6f53f1b326d9642fd31e4495b2c94d8474909462
ftorrejon <ftorrejon@AT-8J1V5M2.atsistemas.lan>
2018-02-07 10:42:17 +0100

    Servicio de Ventas

--- src/main/java/atsistemas/prueba/service/Sale/SaleService.java
+++ src/main/java/atsistemas/prueba/service/Sale/SaleService.java
@@ -1,5 +1,25 @@
 package atsistemas.prueba.service.Sale;
 
+import java.util.List;
+
+import atsistemas.prueba.dto.SaleDto;
+import atsistemas.prueba.model.Sale;
+
 public interface SaleService {
+	
+	public Sale map (SaleDto s);
+	
+	public SaleDto map(Sale s);
+	
+	public List<SaleDto> findAll();
+	
+	public SaleDto findById(Integer id) /*throws NotFoundException*/;
+	
+	public SaleDto create(SaleDto saleDto) /*throws InvalidDataException,*/ /*throws NotFoundException*/;
+
+	public void update(SaleDto saleDto) /*throws NotFoundException*/;
+
+	public void delete(Integer id);
+
 
 }
